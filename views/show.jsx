3f842e2dc5535b2fd3ad7d2f51c1be24669e1190
Eric Lowe <ericlowe25@gmail.ocm>
2022-05-22 22:33:34 -0400

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -17,11 +17,11 @@ function Show ({bread, index}) {
                 }
             </p>
             <img src={bread.image} alt={bread.name}/>
-            <li><a href="/breads">Go home</a></li>
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
          <form action={`/breads/${index}?_method=DELETE`} method="POST">
             <input type='submit' value="DELETE"/>
          </form>
-
+         <a href='/breads'><button className='btn-primary'>Go home</button></a>
         </Default>
     )
 }
